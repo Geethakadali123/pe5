d6d592d051a250b361b01ad6617bb89e6b131f96
Geethakadali123 <45656474+Geethakadali123@users.noreply.github.com>
2019-01-04 12:37:58 +0530

    geetha

---
+++ src/main/java/com/stackroute/Student.java
@@ -0,0 +1,49 @@
+package com.stackroute;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Student {
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private int age;
+    private String name;
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+
+
+
+}
